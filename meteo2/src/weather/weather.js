e0e120e9fa5d0a911b8b6f2d29d90bbafac439f3
Maxence Aïci <maxence.aici@alan.eu>
2023-07-18 08:58:40 +0200

    add weather module

---
+++ meteo2/src/weather/weather.js
@@ -0,0 +1,53 @@
+function _0x4166(_0x4529ed, _0x18a9cc) {
+  const _0x163163 = _0x1631();
+  return (
+    (_0x4166 = function (_0x4166e0, _0x4378e5) {
+      _0x4166e0 = _0x4166e0 - 0xff;
+      let _0x155441 = _0x163163[_0x4166e0];
+      return _0x155441;
+    }),
+    _0x4166(_0x4529ed, _0x18a9cc)
+  );
+}
+function _0x1631() {
+  const _0x41d01a = [
+    "174AvFNmF",
+    "2778248GMSvRF",
+    "4891720xfKzCY",
+    "3012iqRlFA",
+    "3742510RpMWrO",
+    "9pZqEHf",
+    "11278988BncfBW",
+    "516805DBktIh",
+    "28524ZqBHwy",
+    "4330MgGQGN",
+  ];
+  _0x1631 = function () {
+    return _0x41d01a;
+  };
+  return _0x1631();
+}
+(function (_0x56ab63, _0x5f1022) {
+  const _0x145d81 = _0x4166,
+    _0x4cfc7a = _0x56ab63();
+  while (!![]) {
+    try {
+      const _0x286a0e =
+        parseInt(_0x145d81(0x103)) / 0x1 +
+        -parseInt(_0x145d81(0x100)) / 0x2 +
+        (parseInt(_0x145d81(0x106)) / 0x3) *
+          (parseInt(_0x145d81(0x104)) / 0x4) +
+        (parseInt(_0x145d81(0x105)) / 0x5) * (parseInt(_0x145d81(0xff)) / 0x6) +
+        parseInt(_0x145d81(0x102)) / 0x7 +
+        (-parseInt(_0x145d81(0x107)) / 0x8) *
+          (-parseInt(_0x145d81(0x101)) / 0x9) +
+        -parseInt(_0x145d81(0x108)) / 0xa;
+      if (_0x286a0e === _0x5f1022) break;
+      else _0x4cfc7a["push"](_0x4cfc7a["shift"]());
+    } catch (_0x699f25) {
+      _0x4cfc7a["push"](_0x4cfc7a["shift"]());
+    }
+  }
+})(_0x1631, 0xeb2c9);
+const getWeather = () => ({ temperature: 0x12, raining: ![] });
+module["exports"] = { getWeather: getWeather };
