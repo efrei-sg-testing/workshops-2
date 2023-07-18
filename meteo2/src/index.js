f5987f0c5697a37d18ad06083234893780a89ac9
Moshigashi <alexandre.marie@efrei.net>
2023-07-18 14:02:04 +0200

    exercice meteo2 et tests

--- meteo2/src/index.js
+++ meteo2/src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { WeatherShow } from "./weather/WeatherShow"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <h1>Hello</h1>
+    <WeatherShow/>
   </React.StrictMode>,
 );
