6086b47d8dcdfe8a1cd5f3a1d1b6906b5c8fb204
monadimustafa <monadi.mustafa@gmail.com>
2023-07-18 14:21:13 +0000

    exosmeteo

--- meteo2/src/index.js
+++ meteo2/src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import WeatherApp from "./weather/weatherApp";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <h1>Hello</h1>
+    <WeatherApp/>
   </React.StrictMode>,
 );
